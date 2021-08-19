f5284a259db3f47137de8b3e554016211043b870
Adrian Sampson <adrian@radbox.org>
2021-08-19 16:15:31 -0400

    Version bump: v1.2.1

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def _read(fn):
 
 
 setup(name='pyacoustid',
-      version='1.2.0',
+      version='1.2.1',
       description=('bindings for Chromaprint acoustic fingerprinting and the '
                    'Acoustid API'),
       author='Adrian Sampson',
